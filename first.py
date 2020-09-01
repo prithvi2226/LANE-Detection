954591ef750dc6cb9a3cb093ca4655ea8ec0d4d5
Prithvi Prathapan <itsmeprithvi2000@gmail.com>
2020-09-01 23:28:44 +0530

    slow

--- first.py
+++ first.py
@@ -6,5 +6,5 @@ cap = cv2.VideoCapture('Lane.mp4')
 while True:
     success, img = cap.read()
     cv2.imshow("Lane Detection", img)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    if cv2.waitKey(10) & 0xFF == ord('q'):
         break
