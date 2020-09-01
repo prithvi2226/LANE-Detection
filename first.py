c1593d53e8982b46dbc875cd7898033215cfcef5
Prithvi Prathapan <itsmeprithvi2000@gmail.com>
2020-09-01 23:16:37 +0530

    trying to play the video

--- first.py
+++ first.py
@@ -1,1 +1,11 @@
-import cv2
+import cv2 as cv
+import numpy as np
+
+
+cap = cv.VideoCapture('/Lane.mp4')
+while(cap.isOpened()):
+    ret, frame = cap.read()
+    #frame = lanesDetection(frame)
+    cv.imshow('Lanes Detection', frame)
+    if cv.waitKey(1) & 0xFF == ord('q'):
+        break
